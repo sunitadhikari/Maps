ae5a69345a1c51d3fe549ff038fc2d23b01c8202
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-28 13:11:41 +0545

    Kgj

--- map.js
+++ map.js
@@ -1,4 +1,11 @@
 console.log("this is console");
 const myMap = new Map();
+// const key1 ="myStr"
+const key1 = 'myStr' , key2 = {}, key3 = function(){};
+myMap.set(key1, 'This is a string');
+myMap.set(key2, 'This is a blank obj');
+myMap.set(key3, 'This is a empty function');
 console.log(myMap);
-const key1 ="myStr"
+let value1= myMap.get(key2);
+console.log(value1);
+console.log(myMap.size)

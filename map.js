4212e566800304deca4fd1be4e860fced58f6531
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-30 23:44:07 +0545

    gjkj

---
+++ map.js
@@ -0,0 +1,10 @@
+function nam(love){
+    if(love){
+        let name = "sunita";
+        const sname="Pokharel";
+    //     console.log("my name is " + name+ " " + sname);
+    // }
+}
+    console.log("my name is " + name+ " " + sname);
+}
+nam(true);

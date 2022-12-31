5da161c4d65042baeeaddc8b1c6481d16fddc7a3
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-31 15:48:02 +0545

    jkgkJ

--- map.js
+++ map.js
@@ -1,2 +1,3 @@
 const myprogram=['js', 'c', 'python', 'c++'];
-console.log("my fav prog lang is " + myprogram)
+console.log("my fav prog lang is " + myprogram);
+console.log(`my fav prog lang is  ${myprogram}`);

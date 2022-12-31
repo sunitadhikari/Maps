8020f9a6595d91c09d6f12991fc09232d8119c16
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-31 13:01:22 +0545

    kgj

--- map.js
+++ map.js
@@ -17,5 +17,6 @@ console.log(`my name is ${firstName}.my last name is ${lastName}`);
 let a = 20;
 let b = 30;
 console.log(`fifty is  ${(a + b)}  and
-    not  ${(2*a+b)}.`  );
-    
+    not  ${(2*a+b)}.`.endsWith('f'));
+console.log(`${firstName}`. includes('ta'));
+    console.log(`${firstName}${lastName}  `.repeat(19))

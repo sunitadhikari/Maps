b28cb88e2d3fd10b9408dbd2201dcedca6dec716
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-31 12:30:21 +0545

    gjjk

--- map.js
+++ map.js
@@ -1,10 +1,14 @@
-function nam(love){
-    if(love){
-        let name = "sunita";
-        const sname="Pokharel";
-    //     console.log("my name is " + name+ " " + sname);
-    // }
-}
-    console.log("my name is " + name+ " " + sname);
-}
-nam(true);
+// function nam(love){
+//     if(love){
+//         let name = "sunita";
+//         const sname="Pokharel";
+//     //     console.log("my name is " + name+ " " + sname);
+//     // }
+// }
+//     console.log("my name is " + name+ " " + sname);
+// }
+console.log("my name is sunita" + "my age is 23");
+console.log(`my name is sunita my age is 23`);
+let firstName  = "sunita";
+let lastName = "Adhikrai";
+console.log("my name is " + firstName + " my last name is "  + lastName);

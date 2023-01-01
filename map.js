679eac3e865b140b48d82a83773410c01ea4db02
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-01 21:49:24 +0545

    jgjk

--- map.js
+++ map.js
@@ -1,10 +1,6 @@
-let a=5;
-let b=10;
-// let temp = a;
-// console.log(a);
-// a= b;
-// b=temp;
-// console.log(`the value of a is ${a} and the value of b is ${b}`);
-
-[a,b]=[b,a];
-    console.log(`the value of a is ${a}  and value of b is ${b}`)
+const bioData={
+    name:"Sunita",
+    age :26,
+    deg:bachlor
+}
+console.log(`my name is ${bioData.name}`);

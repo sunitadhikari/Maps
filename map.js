a6f5cf9ffe01a1ebb87cfe943ac4369820512149
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-01 22:23:15 +0545

    kjj

--- map.js
+++ map.js
@@ -2,6 +2,11 @@ const bioData={
     name:"Sunita",
     age :26,
     deg:"bachlor",
+    hobb:{
+        first:'playing',
+        sec:'youtune',
+    }
 }
+
 let{name,age,deg}=bioData;
-console.log(`my name is ${bioData}`);
+console.log(`my name is ${bioData} i love making ${hobb.sec}`);

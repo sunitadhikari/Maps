3d0e2e9d7880a6d540a77af7fcf316d2e1fa0625
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-01 21:37:12 +0545

    kjk

--- map.js
+++ map.js
@@ -1,7 +1,10 @@
 let a=5;
 let b=10;
-let temp = a;
-console.log(a);
-a= b;
-b=temp;
-console.log(`the value of a is ${a} and the value of b is ${b}`);
+// let temp = a;
+// console.log(a);
+// a= b;
+// b=temp;
+// console.log(`the value of a is ${a} and the value of b is ${b}`);
+
+[a,b]=[b,a];
+    console.log(`the value of a is ${a}  and value of b is ${b}`)

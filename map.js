b72cbb7b8ca0c0febbe3da934208ffa07b72c497
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-02 21:11:01 +0545

    kjj

--- map.js
+++ map.js
@@ -1,4 +1,9 @@
 function sum(...inputs){
     console.log(inputs);
+    let total = 0;
+    for(let i of inputs){
+       total += i;
+    }
+    console.log(total);
 }
-sum(1,2,3,4,5,6);
+sum(1,2,3,4,5,6); 

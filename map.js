e15add124a4b950305d14da4fa3c8e6777f6ac03
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-03 21:27:42 +0545

    jgjk

--- map.js
+++ map.js
@@ -1,18 +1,5 @@
-// function sum(a,b,c){
-//     console.log(a+b+c);
-// }
-// sum(1,2,3);
-// var arrVal=[1,2,3];
-// sum.apply(null,arrVal);
-
-// function sum(a,b,c){
-//     console.log(a+b+c);
-// }
-// var arrVal=[5,6,7];
-// sum(...arrVal);
-let arr1=[1,2,3];
-let arr2=[5,6,3];
-let arr3=[8,4,5]
-// arr1=arr1.concat(arr2);
-arr1=[...arr1,...arr2,...arr3];
-console.log(arr1);
+let arr1=[2,4,5,7];
+// let arr2 =arr1;
+// arr2.push(6,4);
+let arr2 =[...arr1,4,5];
+console.log(arr2);

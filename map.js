de33e45853edeafb320e0b8f1b91f164720165d7
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-03 09:33:13 +0545

    gjk

--- map.js
+++ map.js
@@ -1,5 +1,13 @@
+// function sum(a,b,c){
+//     console.log(a+b+c);
+// }
+// sum(1,2,3);
+// var arrVal=[1,2,3];
+// sum.apply(null,arrVal);
+
 function sum(a,b,c){
     console.log(a+b+c);
 }
-sum(1,2,3);
-var arrVal=[1];
+
+var arrVal=[5,6,7];
+sum(...arrVal);

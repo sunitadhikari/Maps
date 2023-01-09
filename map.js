7c58b551974d512265b2e375fc78e59dc1226947
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-09 13:34:48 +0545

    Kgkj

--- map.js
+++ map.js
@@ -1,2 +1,14 @@
-// let a=5, b=3;
-console.log(10**7);
+class Students{
+constructor(name,age){
+    this.myname=name;
+}
+    biodata(){
+        console.log('Hi i am in class');
+        // console.log('Hi i s');
+    }
+}
+let obj1=new Students();
+// let obj2=new Students();
+obj1.biodata();
+
+// obj2.biodata();

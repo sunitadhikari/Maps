f1f45986b8b80e761f75d4e1f8dd99e44d25f8e9
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-09 15:49:04 +0545

    Gjk

--- map.js
+++ map.js
@@ -1,29 +1,5 @@
-// class hello{
-//     constructor(x, y){
-//         this.x=x;
-//         this.y=y;
-//     }
-// }
-// const a=new hello(10, 30);
-// console.log(a.x);
-// console.log(a.y);
-class Students{
-    constructor(name,age){
-        this.myname=name;
-        this.myage=age;
-    }
-     biodata(){
-        console.log(`hi i am a class ${this.myname} is ${this.myage}`);
-    }
+async function getData(){
+    const rollnodata = await proj1;
+    console.log(rollnodata);
 }
-    class player extends Students{
-    constructor(game,name,age){
-        this.mygame=game;
-        super(name,age);
-    }
-}
-let a=new Students('sunita', 22)
-a.biodata();
-a.biodata();
-
-a.biodata();
+getData();

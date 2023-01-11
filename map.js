187052262edbef2ec61c87ecd1a9f9d6b589a987
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-11 20:59:54 +0545

    kgj

--- map.js
+++ map.js
@@ -2,7 +2,6 @@ async function late(){
     const promise = new Promise((resolve, reject)=>{
         setTimeout(()=>{
             resolve("hello its resolve");
-        }, 2000);
+        };
     });
- 
 }

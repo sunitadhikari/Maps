5c15e9af21a8aec9232319609de7bebd280669d4
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-13 12:36:37 +0545

    jkg

--- map.js
+++ map.js
@@ -16,4 +16,21 @@ const validate = ()=>{
     const phone=phone.value.trim();
     const password=password.value.trim();
     const cpassword=cpassword.value.trim();
+
+    if(username===""){
+        setErrormsg(username, 'username cannot be blank');
+    }
+    else if(username.length <= 2){
+        setErrormsg(username, 'username min in 3 char');
+    }else{
+        setSuccessMsg(username);
+    }
+    if(username===""){
+        setErrormsg(username, 'username cannot be blank');
+    }
+    else if(username.length <= 2){
+        setErrormsg(username, 'username min in 3 char');
+    }else{
+        setSuccessMsg(username);
+    }
 }

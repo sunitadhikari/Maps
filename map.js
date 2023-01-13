9aaac021e528e890804d5e502e11f85a2d68ef34
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-13 12:03:12 +0545

    JGkj

--- map.js
+++ map.js
@@ -1,1 +1,6 @@
-
+const form=document.getElementById('form');
+const username=document.getElementById('username');
+const email=document.getElementById('email');
+const phone=document.getElementById('phone');
+const password=document.getElementById('password');
+const cpassword=document.getElementById('cpassword');

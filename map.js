8cb1e14b453f8583f541f1818750154ccf44dcec
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-13 12:19:05 +0545

    g

--- map.js
+++ map.js
@@ -4,3 +4,16 @@ const email=document.getElementById('email');
 const phone=document.getElementById('phone');
 const password=document.getElementById('password');
 const cpassword=document.getElementById('cpassword');
+
+//add event
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    ValidityState();
+})
+const validate = ()=>{
+    const username=username.value.trim();
+    const email=email.value.trim();
+    const phone=phone.value.trim();
+    const password=password.value.trim();
+    const cpassword=cpassword.value.trim();
+}
